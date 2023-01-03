9675d9c117e8625746d5d98c62f4c6f937983c1d
Chris Rosenberger <jcrosenberger@pm.me>
2023-01-03 17:08:21 -0600

    created function to get dataframe for curriculum pages

--- acquire.py
+++ acquire.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 
 
-##------------------------Acquire----------------------##
-
+###############################################################
+  ##############       Primary Function       ##############
+#######  slices txt file and returns logs in dataframe  #######
+###############################################################
 def acquire_curriculum_data():
     '''This function gets curriculum data from the local repo, which is stored
     as a .txt file'''
@@ -11,3 +13,22 @@ def acquire_curriculum_data():
     df = pd.read_table("anonymized-curriculum-access.txt", sep = '\s', header = None, 
                    names = ['date', 'time', 'page', 'id', 'cohort', 'ip'])
     return df
+
+
+
+  ######################################################
+####### formats dataframe for exploring curriculum #######
+  ######################################################
+
+def curriculum_dataframe():
+    '''
+    This function takes in the 'anonymized-curriculum-access' dataframe and returns
+    a dataframe with unique names for pages which were accessed on the server
+    
+    '''
+    df = acquire_curriculum_data()
+    endpoint = df['page'].unique()
+    curriculum_df = pd.DataFrame(endpoint)
+    curriculum_df.rename(columns={0: 'pages'})
+
+    return curriculum_df 
